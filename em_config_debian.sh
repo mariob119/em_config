5e0d5cdc34675e0808c2b081469d54fe3374b6ba
mariob119 <114176085+mariob119@users.noreply.github.com>
2022-11-14 23:19:58 +0100

    Edited docker config

--- em_config_debian.sh
+++ em_config_debian.sh
@@ -77,8 +77,9 @@ echo ""
 echo "Set up everything for docker!"
 echo ""
 
-sudo mkdir -p /etc/apt/keyrings -y
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg
+sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
