d501de447c97c1eecff7cd3776d509b92b659462
mariob119 <114176085+mariob119@users.noreply.github.com>
2022-11-14 20:00:18 +0100

    Added docker installation

--- em_config.sh
+++ em_config.sh
@@ -32,3 +32,22 @@ apt-get install \
 echo ""
 echo "======================================================================"
 echo ""
+
+echo "Set up everything for docker!"
+echo ""
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get update
+
+echo ""
+echo "======================================================================"
+echo ""
+
+echo "Install docker!"
+echo ""
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
