1a551f214668203a180f3709b55b323e60b2bdb7
mariob119 <114176085+mariob119@users.noreply.github.com>
2022-11-15 00:38:21 +0100

    Changed view and docker config

--- em_config_debian.sh
+++ em_config_debian.sh
@@ -109,29 +109,34 @@ sudo apt-get update
 
 if [[ ! -d /etc/apt/keyrings ]]; then
   sudo mkdir -p /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/debian/gpg
-  sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-fi
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-echo \
+  echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+  sudo apt-get update
+fi
 
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-sudo apt-get update
+echo "Docker is prepared!"
 
 echo ""
 echo "======================================================================"
 echo ""
 
-echo "Install docker!"
-echo ""
-sudo apt-get install docker-ce -y
-sudo apt-get docker-ce-cli -y
-sudo apt-get containerd.io -y
-sudo apt-get docker-compose-plugin -y
+if [[ ! -d /verifyinstall/docker ]]; then
+  echo "Install docker!"
+  echo ""
+  sudo apt-get install docker-ce -y
+  sudo apt-get docker-ce-cli -y
+  sudo apt-get containerd.io -y
+  sudo apt-get docker-compose-plugin -y
+  cd /.
+  mkdir verifyinstall/docker
+fi
+
+echo "Docker is installed!"
 
 echo ""
 echo "======================================================================"
