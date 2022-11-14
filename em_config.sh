6fba329607a6ca8d2e967e6ed7782e94c1b0958b
mariob119 <114176085+mariob119@users.noreply.github.com>
2022-11-14 21:29:59 +0100

    Edited portainer config

--- em_config.sh
+++ em_config.sh
@@ -71,10 +71,10 @@ echo ""
 echo "Install Portainer"
 echo ""
 
-docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name portainer \
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
 --restart=always \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -v portainer_data:/data \
-portainer/portainer-ce:latest
+portainer/portainer-ce:2.9.3
 
 docker ps
