3ea99b219a561195197a0993376539e05db3142a
mariob119 <114176085+mariob119@users.noreply.github.com>
2022-11-14 19:45:42 +0100

    Try adding docker support

--- em_config.sh
+++ em_config.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# Script for setting up slim linux for raspberry py
+
+echo "Do update"
 apt-get update
-echo "Hello"
+echo ""
+
+echo "Do upgrade"
 apt-get upgrade -y
+echo ""
+
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
