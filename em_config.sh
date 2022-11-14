a8e5af83ca12150186580a013199ecd54c630b76
mariob119 <114176085+mariob119@users.noreply.github.com>
2022-11-14 19:56:47 +0100

    Added some applications

--- em_config.sh
+++ em_config.sh
@@ -3,18 +3,32 @@
 # Script for setting up slim linux for raspberry py
 
 echo ""
-echo "==========================================================="
+echo "======================================================================"
 
 echo "Do update"
+echo ""
 apt-get update
-echo ""
-echo "==========================================================="
 
 echo ""
+echo "======================================================================"
+echo ""
+
 echo "Do upgrade"
+echo ""
 apt-get upgrade -y
+
+echo ""
+echo "======================================================================"
 echo ""
 
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+echo "Install needed applications"
+echo ""
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+echo ""
+echo "======================================================================"
+echo ""
