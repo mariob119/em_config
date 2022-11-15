3f3336abe233ef047f1b11affcbb36ea3f39ea02
mariob119 <114176085+mariob119@users.noreply.github.com>
2022-11-15 01:11:21 +0100

    Added -y argument for gpg docker

--- em_config_debian.sh
+++ em_config_debian.sh
@@ -95,7 +95,7 @@ echo ""
 sudo apt-get update
 
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg -y | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
 
 echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
